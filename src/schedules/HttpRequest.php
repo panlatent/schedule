d360dcbe44910361fe8bb927f5c11a41a7d5436c
panlatent <panlatent@gmail.com>
2019-04-30 17:00:29 +0800

    Update HttpRequest schedule

--- src/schedules/HttpRequest.php
+++ src/schedules/HttpRequest.php
@@ -12,6 +12,7 @@ use Craft;
 use craft\helpers\Json;
 use GuzzleHttp\Client;
 use panlatent\schedule\base\Schedule;
+use panlatent\schedule\db\Table;
 use Psr\Http\Message\ResponseInterface;
 
 /**
@@ -138,11 +139,21 @@ class HttpRequest extends Schedule
         $response = $client->{$this->method}($this->url, $options);
         $statusCode = $response->getStatusCode();
 
-        $successful =  $statusCode >= 200 && $statusCode < 300;
+        $successful = $statusCode >= 200 && $statusCode < 300;
         if ($successful) {
             Craft::info("Http Request Schedule: a http request has been sent to {$this->url}({$statusCode}) successfully.", __METHOD__);
         } else {
             Craft::warning("Http Request Schedule: a http request has been sent to {$this->url}({$statusCode}) failed.", __METHOD__);
+        }
+
+        if ($logId) {
+            Craft::$app->getDb()->createCommand()
+                ->update(Table::SCHEDULELOGS, [
+                    'output' => (string)$response->getBody(),
+                ], [
+                    'id' => $logId,
+                ])
+                ->execute();
         }
 
         return $successful;
