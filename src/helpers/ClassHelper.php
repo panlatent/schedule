2aed203dd13b3563eaf1b93a01eb2dd63e69d9ad
panlatent <panlatent@gmail.com>
2019-08-08 13:26:20 +0800

    Advance edit queue schedule settings
    Add queue component ID and job class auto suggestions

---
+++ src/helpers/ClassHelper.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Schedule plugin for CraftCMS 3
+ *
+ * @link      https://panlatent.com/
+ * @copyright Copyright (c) 2019 panlatent@gmail.com
+ */
+
+namespace panlatent\schedule\helpers;
+
+use Composer\Autoload\ClassLoader;
+use Composer\Factory;
+use Composer\IO\NullIO;
+use Craft;
+
+/**
+ * Class ClassHelper
+ *
+ * @package panlatent\schedule\helpers
+ * @author Panlatent <panlatent@gmail.com>
+ */
+class ClassHelper
+{
+    /**
+     * Returns known component classes.
+     *
+     * @return string[]
+     */
+    public static function findClasses(): array
+    {
+        // See if Composer has an optimized autoloader
+        // h/t https://stackoverflow.com/a/46435124/1688568
+        $autoloadClass = null;
+        foreach (get_declared_classes() as $class) {
+            if (strpos($class, 'ComposerAutoloaderInit') === 0) {
+                $autoloadClass = $class;
+                break;
+            }
+        }
+
+        if ($autoloadClass !== null) {
+            // Get a list of namespaces we care about
+            $namespaces = ['craft'];
+            foreach (Craft::$app->getPlugins()->getAllPlugins() as $plugin) {
+                $classParts = explode('\\', get_class($plugin));
+                if (count($classParts) > 1) {
+                    $namespaces[] = implode('\\', array_slice($classParts, 0, -1));
+                }
+            }
+
+            $jsonPath = Craft::$app->getComposer()->getJsonPath();
+            $composer = (new Factory())->createComposer(new NullIO(), $jsonPath);
+            $autoload = $composer->getPackage()->getAutoload();
+            if (!empty($autoload['psr-4'])) {
+                foreach (array_keys($autoload['psr-4']) as $namespace) {
+                    $namespaces[] = rtrim($namespace, '\\');
+                }
+            }
+
+            $namespaces = array_unique($namespaces);
+
+            /** @var ClassLoader $classLoader */
+            /** @noinspection PhpUndefinedMethodInspection */
+            $classLoader = $autoloadClass::getLoader();
+            try {
+                foreach ($classLoader->getClassMap() as $class => $file) {
+                    if (!class_exists($class, false) &&
+                        !interface_exists($class, false) &&
+                        !trait_exists($class, false) &&
+                        file_exists($file) &&
+                        substr($class, -4) !== 'Test' &&
+                        substr($class, -8) !== 'TestCase') {
+                        // See if it's in a namespace we care about
+                        foreach ($namespaces as $namespace) {
+                            if (strpos($class, $namespace . '\\') === 0) {
+                                /** @noinspection PhpIncludeInspection */
+                                require $file;
+                                break;
+                            }
+                        }
+                    }
+                }
+            } catch (\Throwable $e) {
+            }
+
+        }
+
+        $classes = get_declared_classes();
+        sort($classes);
+
+        return $classes;
+    }
+
+    /**
+     * @param string $doc
+     * @return string|null
+     */
+    public static function getPhpDocSummary(string $doc)
+    {
+        foreach (preg_split("/\r\n|\n|\r/", $doc) as $line) {
+            $line = preg_replace('#^[/*\s]*(?:@event\s+[^\s]+\s+)?#', '', $line);
+            if (strpos($line, '@') === 0) {
+                return null;
+            }
+            if ($line) {
+                return $line;
+            }
+        }
+
+        return null;
+    }
+}
--- src/schedules/Queue.php
+++ src/schedules/Queue.php
@@ -8,10 +8,14 @@
 
 namespace panlatent\schedule\schedules;
 
+use Closure;
 use Craft;
 use craft\queue\QueueInterface;
 use panlatent\schedule\base\Schedule;
 use panlatent\schedule\errors\ScheduleException;
+use panlatent\schedule\helpers\ClassHelper;
+use ReflectionClass;
+use yii\queue\JobInterface;
 
 /**
  * Class JobSchedule
@@ -61,8 +65,24 @@ class Queue extends Schedule
      */
     public function getSettingsHtml()
     {
+        $componentSuggestions = [
+            [
+                'label' => '',
+                'data' => $this->_getComponentSuggestions(),
+            ]
+        ];
+
+        $jobClassSuggestions = [
+            [
+                'label' => '',
+                'data' => $this->_getJobClassSuggestions(),
+            ]
+        ];
+
         return Craft::$app->getView()->renderTemplate('schedule/_components/schedules/Queue', [
             'schedule' => $this,
+            'componentSuggestions' => $componentSuggestions,
+            'jobClassSuggestions' => $jobClassSuggestions,
         ]);
     }
 
@@ -90,5 +110,58 @@ class Queue extends Schedule
         Craft::info("Queue Schedule push a job: {$this->jobClass} to {$this->componentId} component.", __METHOD__);
 
         return true;
+    }
+
+    // Private Methods
+    // =========================================================================
+
+    /**
+     * @return array
+     */
+    private function _getComponentSuggestions(): array
+    {
+        $suggestions = [];
+
+        $components = Craft::$app->getComponents();
+        ksort($components);
+
+        foreach ($components as $id => $component) {
+            $hint = '';
+            if (is_object($component) && !$component instanceof Closure) {
+                $hint = get_class($component);
+            } elseif (!is_callable($component) && is_array($component) && !empty($component['class'])) {
+                $hint = $component['class'];
+            }
+
+            $suggestions[] = [
+                'name' => $id,
+                'hint' => $hint,
+            ];
+        }
+
+        return $suggestions;
+    }
+
+    /**
+     * @return array
+     */
+    private function _getJobClassSuggestions(): array
+    {
+        $suggestions = [];
+        foreach (ClassHelper::findClasses() as $class) {
+            if (is_subclass_of($class, JobInterface::class)) {
+                $reflection = new ReflectionClass($class);
+                if (!$reflection->isInstantiable()) {
+                    continue;
+                }
+
+                $suggestions[] = [
+                    'name' => $class,
+                    'hint' => ClassHelper::getPhpDocSummary($reflection->getDocComment()),
+                ];
+            }
+        }
+
+        return $suggestions;
     }
 }
